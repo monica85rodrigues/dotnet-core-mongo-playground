a9485ba9f659bc360d50126ed89eeea80e1a032f
Mónica Rodrigues <monica85rodrigues@gmail.com>
2021-04-26 23:16:42 +0100

    Initial commit

---
+++ dotnet-core-mongo/Api/Controllers/BookmarksController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/bookmarks")]
+    public class BookmarksController : ControllerBase
+    {
+        private static readonly ConcurrentDictionary<Guid, Bookmark> _bookmarks = new();
+        
+        private readonly ILogger<BookmarksController> _logger;
+
+        public BookmarksController(ILogger<BookmarksController> logger)
+        {
+            _logger = logger;
+
+            var id = Guid.NewGuid();
+            _bookmarks.TryAdd(id, new Bookmark {Id = id, Name = "bookmark 1"});
+            id = Guid.NewGuid();
+            _bookmarks.TryAdd(id, new Bookmark {Id = id, Name = "bookmark 2"});
+            id = Guid.NewGuid();
+            _bookmarks.TryAdd(id, new Bookmark {Id = id, Name = "bookmark 3"});
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetALlBookmarks()
+        {
+            return Ok(await Task.FromResult(_bookmarks.Values));
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBookmarkById(Guid id)
+        {
+            if (_bookmarks.TryGetValue(id, out Bookmark bookmark))
+            {
+                return Ok(await Task.FromResult(bookmark));
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ dotnet-core-mongo/Api/Models/Bookmark.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Api.Models
+{
+    public class Bookmark
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Url { get; set; }
+    }
+}
